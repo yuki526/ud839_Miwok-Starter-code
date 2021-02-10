8f36b677c1fe0029b14de41e2a3e393135f98598
yuki526 <yk.g187@gmail.com>
2021-02-10 21:58:41 +0900

    add for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,15 +1,21 @@
 package com.example.android.miwok;
 
+import android.annotation.SuppressLint;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +34,13 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        ConstraintLayout rootView = (ConstraintLayout) findViewById(R.id.rootView);
-        TextView wordView = new TextView(this);
-        wordView.setText(words.get(0));
-        rootView.addView(wordView);
+
+        for (String word : words ) {
+            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+            TextView wordView = new TextView(this);
+            wordView.setText(word);
+            rootView.addView(wordView);
+        }
     }
 }
+
