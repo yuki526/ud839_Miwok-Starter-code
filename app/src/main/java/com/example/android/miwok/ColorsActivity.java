110836a7187468042bad89ec78d85a66c18d9a6e
yuki526 <yk.g187@gmail.com>
2021-02-14 14:30:01 +0900

    modified FamilyActivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,13 +1,52 @@
 package com.example.android.miwok;
 
+import android.annotation.SuppressLint;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColorsActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
+//        for (String word : words ) {
+//            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//            TextView wordView = new TextView(this);
+//            wordView.setText(word);
+//            rootView.addView(wordView);
+//        }
     }
 }
+
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,13 +1,53 @@
 package com.example.android.miwok;
 
+import android.annotation.SuppressLint;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FamilyActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother ", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
+//        for (String word : words ) {
+//            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//            TextView wordView = new TextView(this);
+//            wordView.setText(word);
+//            rootView.addView(wordView);
+//        }
     }
 }
+
